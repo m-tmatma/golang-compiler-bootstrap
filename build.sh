9de4936ca4e0a9dfcd076048864d0f5b80d648f3
Masaru Tsuchiyama <m.tmatma@gmail.com>
2025-08-03 14:51:52 +0900

    go1.24 に対応

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@
 (export GOROOT_BOOTSTRAP=$(pwd)/go1.4  && cd go1.17/src    && ./make.bash )
 (export GOROOT_BOOTSTRAP=$(pwd)/go1.17 && cd go1.20/src    && ./make.bash )
 (export GOROOT_BOOTSTRAP=$(pwd)/go1.20 && cd go1.22/src    && ./make.bash )
-(export GOROOT_BOOTSTRAP=$(pwd)/go1.22 && cd go-latest/src && ./make.bash )
+(export GOROOT_BOOTSTRAP=$(pwd)/go1.22 && cd go1.24/src    && ./make.bash )
+(export GOROOT_BOOTSTRAP=$(pwd)/go1.24 && cd go-latest/src && ./make.bash )
 
--- clone.sh
+++ clone.sh
@@ -4,5 +4,6 @@ git clone -b release-branch.go1.4  https://github.com/golang/go go1.4
 git clone -b release-branch.go1.17 https://github.com/golang/go go1.17
 git clone -b release-branch.go1.20 https://github.com/golang/go go1.20
 git clone -b release-branch.go1.22 https://github.com/golang/go go1.22
+git clone -b release-branch.go1.24 https://github.com/golang/go go1.24
 git clone                          https://github.com/golang/go go-latest
 
